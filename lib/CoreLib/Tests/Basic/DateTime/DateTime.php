bc64b6aeca40298a08741aab981219e8716f8af1
SplitIce <mat999@gmail.com>
2012-04-01 16:50:59 +1000

    DateTime tests

---
+++ lib/CoreLib/Tests/Basic/DateTime/DateTime.php
@@ -0,0 +1,15 @@
+<?php
+namespace Tests\Basic\DateTime;
+
+use Debug\Test\IUnitTest;
+use Debug\Test\Unit;
+
+class DateTime extends Timestamp {
+	function testToFromSQL(){
+		$date = '2012-04-01';
+		
+		$dt = \Basic\DateTime\DateTime::fromSQL($date);
+		$this->assertEqual(strtotime($date),(string)$dt, 'DateTime fromSQL');
+		$this->assertEqual($date,$dt->toSQL(), 'DateTime toSQL');
+	}
+} // End arr
