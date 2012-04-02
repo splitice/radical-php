a0bbe79b7212904ee31224d4fe5138de1efe5950
SplitIce <mat999@gmail.com>
2012-04-02 17:47:20 +1000

    Common File New Layout support

---
+++ system/config.php
@@ -0,0 +1,2 @@
+<?php
+//Default Config
--- system/include/common.php
+++ system/include/common.php
@@ -3,24 +3,28 @@ define ( 'DS', DIRECTORY_SEPARATOR );
 $BASEPATH = rtrim(dirname ( realpath($_SERVER ["SCRIPT_NAME"].'/../') ),'/') . '/';
 
 //Check PHP Version
-if (version_compare (PHP_VERSION, '5.3.0b1-dev') < 0)
+if (version_compare (PHP_VERSION, '5.3.3') < 0)
 {
-	die ("You need PHP 5.3 or higher to run this script.\n");
+	die ("You need PHP 5.3.3 or higher to run this script.\n");
 }
 
-$user_include = __DIR__.DS.'..'.DS.'user_include'.DS;
+$user_dir = __DIR__.DS.'..'.DS.'..'.DS;
 if(!file_exists($user_include)){
 	$user_include = null;
 }
+$user_include = $user_dir . DS . 'include' . DS;
 
 include (__DIR__ . '/autoloader.php');
 include (__DIR__ . '/functions.php');
-if($user_include && file_exists($user_include.'before_config.php')){
-	include($user_include.'before_config.php');
-}
-include (__DIR__ . '/../config.php');
 
-DB::Connect ( $_SQL );
+include (__DIR__ . '/../config.php');
+if($user_include && file_exists($user_dir.'config.php')){
+	include($user_dir.'config.php');
+}
+
+if(isset($_SQL)){
+	DB::Connect ( $_SQL );
+}
 
 Web\Session::Init ();
 
