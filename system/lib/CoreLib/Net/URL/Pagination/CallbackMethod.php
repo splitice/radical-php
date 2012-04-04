b7827de8c5362ed9e81de9d270e34ffdfe938baf
SplitIce <mat999@gmail.com>
2012-04-04 23:32:27 +1000

    Callback Paginator

---
+++ system/lib/CoreLib/Net/URL/Pagination/CallbackMethod.php
@@ -0,0 +1,9 @@
+<?php
+namespace Net\URL\Pagination;
+
+class CallbackMethod extends Internal\PaginationBase implements IPaginator {
+	function toURL($page = 1){
+		$callback = $this->url;
+		return $callback($page);
+	}
+}
