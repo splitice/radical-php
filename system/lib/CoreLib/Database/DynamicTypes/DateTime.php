8ede838cde6833a0cc2bea43bb38e7e5da3f650b
SplitIce <mat999@gmail.com>
2012-04-15 23:50:15 +1000

    Fix DateTime Dynamic Type. timestamp is the variable not value.

--- system/lib/CoreLib/Database/DynamicTypes/DateTime.php
+++ system/lib/CoreLib/Database/DynamicTypes/DateTime.php
@@ -4,19 +4,18 @@ namespace Database\DynamicTypes;
 use Database\Model\ITable;
 
 class DateTime extends \Basic\DateTime\DateTime implements IDynamicType {
-	protected $value;
 	protected $extra;
 	
 	/**
 	 * @param string $value
 	 */
 	public function setValue($value) {
-		$this->value = $value;
+		$this->timestamp = $value;
 	}
 
 	function __construct($value,$extra){
-		$this->value = $value;
 		$this->extra = $extra;
+		parent::__construct($value);
 	}
 	function __toString(){
 		return (string)$this->toSQL();
