c8338287d3eb1e80a8c00116442ff8c4310ae107
SplitIce <mat999@gmail.com>
2012-04-25 01:18:08 +1000

    Templated PageRecognizer bug fix.

--- system/lib/CoreLib/Web/PageRecogniser/Recognisers/Templates/Standard.php
+++ system/lib/CoreLib/Web/PageRecogniser/Recognisers/Templates/Standard.php
@@ -6,14 +6,12 @@ use \Web\Pages;
 use \Web\PageHandler;
 
 class Standard implements IPageRecognise {
-	static $match = array(
-	//	'/page/%(id)d'=>'\\Web\\Pages\\Page'
-	);
+	static $match = array();
 	static function Recognise(\Net\URL $url){
 		$path = $url->getPath()->getPath(true);
 		foreach(static::$match as $expr=>$class){
 			$match = Format::Consume($path, $expr);
-			if($match !== false){
+			if($match){
 				return new $class($match);
 			}
 		}
