31d7451e5d6ebca78f920855cd614d991972af1b
SplitIce <mat999@gmail.com>
2012-05-22 00:15:52 +1000

    Core Improvements.

--- system/include/common.php
+++ system/include/common.php
@@ -1,33 +1,43 @@
 <?php
 define ( 'DS', DIRECTORY_SEPARATOR );
+
+//Attempt to compute basepath
 if($_SERVER['DOCUMENT_ROOT']){
-	$BASEPATH = $_SERVER['DOCUMENT_ROOT'] . DS;
-}elseif(php_sapi_name() == 'cli' && isset($argv)){
-	$BASEPATH = dirname(dirname(dirname($argv[0]))) . DS;
+	$BASEPATH = $_SERVER['DOCUMENT_ROOT'];
+}elseif(php_sapi_name() == 'cli'){
+	if(isset($argv) && isset($argv[0])){
+		$BASEPATH = dirname(dirname(dirname($argv[0])));
+	}else{
+		$BASEPATH = getcwd();//We guess
+	}
 }else{	
-	$BASEPATH = realpath(__DIR__ . DS . '..') . DS;
+	$BASEPATH = realpath(__DIR__ . DS . '..');
 }
+$BASEPATH .= DS;
 
 //Check PHP Version
 if (version_compare (PHP_VERSION, '5.3.3') < 0)
 {
+	//A basic error as we cant rely on any further
+	//of the script to be lesser version compatible
 	die ("You need PHP 5.3.3 or higher to run this script.\n");
 }
 
 //Path to user directory
-$user_dir = $BASEPATH.'app'.DS;
-if(!($user_dir = realpath($user_dir))){
+$application_dir = $BASEPATH.'app'.DS;
+if(!($application_dir = realpath($application_dir))){
 	die('Application directory not provided.');
 }
-$user_include = $user_dir . DS . 'include' . DS;
+$application_include = $application_dir . DS . 'include' . DS;
 
+//Common includes
 include (__DIR__ . '/autoloader.php');
 include (__DIR__ . '/functions.php');
 
 //Config
-include ($user_dir . '/config.php');
-if($user_include && file_exists($user_dir.'config.php')){
-	include($user_dir.'config.php');
+include ($application_dir . '/config.php');
+if($application_include && file_exists($application_dir.'config.php')){
+	include($application_dir.'config.php');
 }
 
 //Connect SQL if used
@@ -35,8 +45,14 @@ if(isset($_SQL)){
 	DB::Connect ( $_SQL );
 }
 
-Web\Session::Init ();
+//Session Init
+if(php_sapi_name() != 'cli')
+	Web\Session::Init ();
 
-if($user_include && file_exists($user_include.'onload.php')){
-	include($user_include.'onload.php');
+//Application onload hook
+if($application_include && file_exists($application_include.'onload.php')){
+	include($application_include.'onload.php');
 }
+
+//Cleanup
+unset($application_include, $application_dir);
