303c2bb23194badd808f200161fdc3369f4f9cf5
Jacer OMRI <jokerhacker.jacer@gmail.com>
2012-06-21 08:58:03 +0300

    LoginDeatils are devided into UserPass and ApiKey

--- system/lib/CoreLib/Basic/Structs/LoginDetails.php
+++ system/lib/CoreLib/Basic/Structs/LoginDetails.php
@@ -2,25 +2,5 @@
 namespace Basic\Structs;
 
 class LoginDetails extends \Core\Object {
-	protected $username;
-	protected $password;
-	
-	function __construct($username,$password){
-		$this->username = $username;
-		$this->password = $password;
-	}
-	
-	/**
-	 * @return the $username
-	 */
-	public function getUsername() {
-		return $this->username;
-	}
-	
-	/**
-	 * @return the $password
-	 */
-	public function getPassword() {
-		return $this->password;
-	}
+
 }
