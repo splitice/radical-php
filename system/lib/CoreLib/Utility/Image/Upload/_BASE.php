ebdfe918edd6909a27799fce1bb443e61ace091a
Jacer OMRI <jokerhacker.jacer@gmail.com>
2012-06-21 07:33:23 +0200

    Base class for image upload changed to support both UserPass and ApiKey LoginDetails

--- system/lib/CoreLib/Utility/Image/Upload/_BASE.php
+++ system/lib/CoreLib/Utility/Image/Upload/_BASE.php
@@ -2,17 +2,24 @@
 namespace Utility\Image\Upload;
 
 use Basic\Structs\LoginDetails;
+use Basic\Structs\UserPass;
+use Basic\Structs\ApiKey;
 
 abstract class _BASE {
 	public $user;
 	public $pass;
+	public $key;
 	
 	const TYPE_SAFE = 1;
 	const TYPE_ADULT = 2;
 	
 	function __construct(LoginDetails $details){
-		$this->user = $details->getUsername();
-		$this->pass = $details->getPassword();
+		if($details instanceof UserPass) {
+			$this->user = $details->getUsername();
+			$this->pass = $details->getPassword();
+		}
+		elseif ($details instanceof ApiKey)
+			$this->key = $details->getKey;
 	}
 	
 	static function Login(){
