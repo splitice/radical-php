f0a8d7d4f9526872db53f72e1ea63cd0f5d3fb21
SplitIce <mat999@gmail.com>
2012-07-20 10:48:33 +1000

    Fix bug with the comparison of the first object of each model (ORM related).

--- system/lib/CoreLib/Model/Database/ORM/Manager.php
+++ system/lib/CoreLib/Model/Database/ORM/Manager.php
@@ -4,12 +4,16 @@ namespace Model\Database\ORM;
 use Model\Database\Model\TableReferenceInstance;
 
 class Manager {
-	static function getModel(TableReferenceInstance $table){
+	static function getModel(TableReferenceInstance $table, $data = true){
 		//cached
 		$model = Cache::Get($table);
 		if($model) return $model;
 		
 		$model = new Model($table);
+		
+		if($data)
+			$model = $model->toModelData();
+		
 		return $model;
 	}
 }
