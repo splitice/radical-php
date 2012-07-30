23e244a01ac8c36b5ad31b71575b4935b7bf25b0
SplitIce <mat999@gmail.com>
2012-07-30 17:20:03 +1000

    Clean up and work

--- system/lib/Framework/Utility/Net/HTTP/Curl/IOStream.php
+++ system/lib/Framework/Utility/Net/HTTP/Curl/IOStream.php
@@ -18,28 +18,62 @@ class IOStream {
            $this->setErrorOutput(STDERR);
            $this->setHeaderOutput(null);
         }
+        /**
+         * File to send curl output to
+         * 
+         * @param string $file
+         */
         function setTransOutput($file){
             if($file)
                 $this->transferoutput = $file;
         }
 
+        /**
+         * File to read input from
+         * 
+         * @param string $file
+         */
         function setTransInput($file){
             $this->transferinput = $file;
         }
 
+        /**
+         * File to write output to
+         * 
+         * @param string $file
+         */
         function setErrorOutput($file){
             $this->erroroutput = $file;
         }
 
+        /**
+         * File to write headers to
+         * 
+         * @param string $file
+         */
         function setHeaderOutput($file){
             $this->headeroutput = $file;
         }
 
-        function apply(Curl &$curl){
+        /**
+         * Apply to a curl object
+         * 
+         * @param Curl $curl
+         */
+        function apply(Curl $curl){
             $curl->curl[CURLOPT_FILE] = fopen($this->transferoutput, 'w+');
             $curl->curl[CURLOPT_INFILE] = fopen($this->transferinput, 'r');
             $curl->curl[CURLOPT_STDERR] = fopen($this->erroroutput, 'w+');
             $curl->curl[CURLOPT_WRITEHEADER] = fopen($this->headeroutput, 'w+');
+        }
+        
+        function toArray(){
+        	return array(
+        			'transfer_output'=>$this->transferoutput,
+        			'transfer_input'=>$this->transferinput,
+        			'error_output'=>$this->erroroutput,
+        			'header_output'=>$this->headeroutput
+        			);
         }
 
         function __toString(){
