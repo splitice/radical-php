97b71aa688217f0ce308d872775028673931d83a
spltice <splitice@splitice.net>
2012-10-04 18:28:21 +1000

    From fix.

--- system/lib/Framework/Utility/Net/Mail/Handler/Internal.php
+++ system/lib/Framework/Utility/Net/Mail/Handler/Internal.php
@@ -9,13 +9,13 @@ class Internal implements IMailHandler {
 		// To send HTML mail, the Content-type header must be set
 		if($message->getHtml()){
 			$headers .= 'MIME-Version: 1.0' . "\r\n";
-			$headers .= 'Content-type: text/html; charset=iso-8859-1' ;
+			$headers .= 'Content-type: text/html; charset=iso-8859-1'."\r\n" ;
 		}
 		
-		$headers .= 'From: '.$message->getFrom();
+		$headers .= 'From: '.$message->getFrom()."\r\n";
 		
 		if($message->getReplyTo()){
-			$headers .= "\r\n".'Reply-To: '.$message->getReplyTo();	
+			$headers .= 'Reply-To: '.$message->getReplyTo()."\r\n";	
 		}
 		
 		mail($message->getTo(),$message->getSubject(),$body,$headers);
