2fcd0ef26bad27c95eb4b56627fe92675f8bd074
splitice <mat999@gmail.com>
2012-12-23 00:00:49 +1100

    DNSBL

--- system/lib/Framework/Utility/Net/DNSBL.php
+++ system/lib/Framework/Utility/Net/DNSBL.php
@@ -2,5 +2,21 @@
 namespace Utility\Net;
 
 class DNSBL {
+	const LIST_SPAMHAUS = 'sbl.spamhaus.org';
+	const LIST_DSBL = 'list.dsbl.org';
+	const LIST_DSBL = 'bl.spamcop.net';
 	
+	private $list;
+	function __construct($list){
+		$this->list = $list;
+	}
+	
+	function blacklisted($ip){
+		if(!($ip instanceof IP))
+			$ip = new IP($ip);
+		
+		$ip = $ip->reverse();
+		
+		return checkdnsrr($ip . "." . $this->list . ".", "A");
+	}
 }
--- system/lib/Framework/Utility/Net/IP.php
+++ system/lib/Framework/Utility/Net/IP.php
@@ -39,6 +39,11 @@ class IP {
 		return CRC32::Hash($this->ip);
 	}
 	
+	function reverse(){
+		$e = array_reverse(explode('.',$this->ip));
+		return new static(implode('.',$e));
+	}
+	
 	function ping($timeout = 1) {
 		/* ICMP ping packet with a pre-calculated checksum */
 		$package = "\x08\x00\x7d\x4b\x00\x00\x00\x00PingHost";
