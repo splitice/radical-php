e0d3f41913bdf79aac02bf60cd6e241198ad8fec
splitice <mat999@gmail.com>
2012-12-23 18:51:03 +1100

    A quick addition of another dynamicType

---
+++ system/lib/Framework/Model/Database/DynamicTypes/URL.php
@@ -0,0 +1,17 @@
+<?php
+namespace Model\Database\DynamicTypes;
+
+use Exceptions\ValidationException;
+use Model\Database\Model\ITable;
+
+class URL extends String implements IDynamicValidate {
+	function validate($value){
+		$url = \Utility\Net\URL::fromURL($value);
+		if(!$url)
+			return false;
+		return true;
+	}
+	function getUrl(){
+		return \Utility\Net\URL::fromURL($this->value);
+	}
+}
