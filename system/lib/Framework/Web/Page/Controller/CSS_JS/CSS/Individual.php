ed792c9d67c814797c098c17ec8ec34d871cba71
splitice <mat999@gmail.com>
2012-12-30 02:21:50 +1100

    The beginings of SaaS support, TODO: Modularlize... eventuallly.

--- system/lib/Framework/Web/Page/Controller/CSS_JS/CSS/Individual.php
+++ system/lib/Framework/Web/Page/Controller/CSS_JS/CSS/Individual.php
@@ -5,4 +5,24 @@ use Web\Page\Controller\CSS_JS\Internal\IndividualBase;
 class Individual extends IndividualBase {
 	const MIME_TYPE = 'text/css';
 	const EXTENSION = 'css';
+	
+	static function get_file($file){
+		if($ext == 'scss' || $ext == 'sass'){
+			$options = array(
+					'style' => 'nested',
+					'cache' => false,
+					'syntax' => $ext,
+					'debug' => false,
+					'callbacks' => array(
+							'warn' => 'cb_warn',
+							'debug' => 'cb_debug',
+					),
+			);
+			// Execute the compiler.
+			$parser = new \Web\Sass\SassParser($options);
+			return $parser->toCss($file);
+		}
+		
+		return file_get_contents($file);
+	}
 }
---
+++ system/lib/PHPSass/Web/Sass/SassLiteralExceptions.php
@@ -0,0 +1,45 @@
+<?php namespace Web\Sass;
+/* SVN FILE: $Id$ */
+/**
+ * Sass literal exception classes.
+ * @author      Chris Yates <chris.l.yates@gmail.com>
+ * @copyright   Copyright (c) 2010 PBM Web Development
+ * @license      http://phamlp.googlecode.com/files/license.txt
+ * @package      PHamlP
+ * @subpackage  Sass.script.literals
+ */
+
+/**
+ * Sass literal exception.
+ * @package      PHamlP
+ * @subpackage  Sass.script.literals
+ */
+class SassLiteralException extends SassScriptParserException {}
+
+/**
+ * SassBooleanException class.
+ * @package      PHamlP
+ * @subpackage  Sass.script.literals
+ */
+class SassBooleanException extends SassLiteralException {}
+
+/**
+ * SassColourException class.
+ * @package      PHamlP
+ * @subpackage  Sass.script.literals
+ */
+class SassColourException extends SassLiteralException {}
+
+/**
+ * SassNumberException class.
+ * @package      PHamlP
+ * @subpackage  Sass.script.literals
+ */
+class SassNumberException extends SassLiteralException {}
+
+/**
+ * SassStringException class.
+ * @package      PHamlP
+ * @subpackage  Sass.script.literals
+ */
+class SassStringException extends SassLiteralException {}
